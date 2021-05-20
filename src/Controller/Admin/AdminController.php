17435db9ae69811f6d40c304b38b5843ebea42dd
aureelienHub <aurelien.thierry@live.fr>
2021-05-20 16:44:16 +0200

    admin_ajout_style

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin_")
      */
     public function index(): Response
     {
