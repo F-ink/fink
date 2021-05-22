bf17d5c67f725b0b620e8870d17531005f33ba08
Gabsy <gfournier-neri@laposte.net>
2021-05-22 17:00:15 +0200

    Gallerie

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -60,4 +60,51 @@ class HomeController extends AbstractController
             'styles' => $styles
         ]);
     }
+
+
+    /**
+     * @Route("/gallerie", name="gallerie")
+     */
+    public function gallerie(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = new Artist();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $styles = $entityManager->getRepository(Style::class)->findAll();
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+
+            // // generate a signed url and email it to the user
+            // $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            //     (new TemplatedEmail())
+            //         ->from(new Address('finkart@outlook.fr', 'Fink Art'))
+            //         ->to($user->getEmail())
+            //         ->subject('Please Confirm your Email')
+            //         ->htmlTemplate('registration/confirmation_email.html.twig')
+            // );
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('_profiler_home');
+        }
+
+
+
+        return $this->render('home/gallerie.html.twig', [
+            'registrationForm' => $form->createView(),
+            'styles' => $styles
+        ]);
+    }
 }
