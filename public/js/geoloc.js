bfefb7e53c32fe2e4b66e971b967e6d809fdc191
aureelienHub <aurelien.thierry@live.fr>
2021-05-27 14:20:17 +0200

    map

--- public/js/geoloc.js
+++ public/js/geoloc.js
@@ -1,29 +1,8 @@
-let $map = document.querySelector('#map')
+let map;
 
-class GoogleMap {
-
-    // constructor() {
-    //     this.map = null
-    //     this.bounds = null
-    //     this.textMarker = null
-    // }
-
-    /**
-     * Charge la carte
-     * @param {HTMLElement} element 
-     */
-    load(element) {
-        $script('https://maps.googleapis.com/maps/api/js', () => {
-            let center = {lat: -25.363, lng: 131.044};
-            let map = new GoogleMap.maps.Map(element, {
-                zoom: 4,
-                center:center
-            });
-        })
-    }
-}
-
-if ($map !==null){
-    let map = new GoogleMap()
-    map.load($map)
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 }
